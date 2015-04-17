fb3547f77f55be9e93ed27f9b9fac04ec4cdbc6c
Shivaraj Shetty <shivaraj@codeaurora.org>
2015-04-18 00:11:22 +0530

    msm: mdss: protect iommu operations with mutex for mdp3
    On mdp3 targets iommu operation is not properly protected
    by mutex lock. This is causing iommu to be detached while
    hardware is fetching the data from the iommu mapped
    address leading to SNOC errors. Use mutex lock properly
    while using the iommu to fix the issue.
    
    Change-Id: Ife59791b08b123f6b60d3ffa6d8061839244934c
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/video/msm/mdss/mdp3.c
+++ drivers/video/msm/mdss/mdp3.c
@@ -699,11 +699,9 @@ int mdp3_iommu_attach(int context)
 	if (context >= MDP3_IOMMU_CTX_MAX)
 		return -EINVAL;
 
-	mutex_lock(&mdp3_res->iommu_lock);
 	context_map = mdp3_res->iommu_contexts + context;
 	if (context_map->attached) {
 		pr_warn("mdp iommu already attached\n");
-		mutex_unlock(&mdp3_res->iommu_lock);
 		return 0;
 	}
 
@@ -712,7 +710,6 @@ int mdp3_iommu_attach(int context)
 	iommu_attach_device(domain_map->domain, context_map->ctx);
 
 	context_map->attached = true;
-	mutex_unlock(&mdp3_res->iommu_lock);
 	return 0;
 }
 
@@ -725,11 +722,9 @@ int mdp3_iommu_dettach(int context)
 		context >= MDP3_IOMMU_CTX_MAX)
 		return -EINVAL;
 
-	mutex_lock(&mdp3_res->iommu_lock);
 	context_map = mdp3_res->iommu_contexts + context;
 	if (!context_map->attached) {
 		pr_warn("mdp iommu not attached\n");
-		mutex_unlock(&mdp3_res->iommu_lock);
 		return 0;
 	}
 
@@ -737,7 +732,6 @@ int mdp3_iommu_dettach(int context)
 	iommu_detach_device(domain_map->domain, context_map->ctx);
 	context_map->attached = false;
 
-	mutex_unlock(&mdp3_res->iommu_lock);
 	return 0;
 }
 
@@ -926,8 +920,15 @@ static int mdp3_res_init(void)
 
 static void mdp3_res_deinit(void)
 {
+	int i;
+
 	mdp3_bus_scale_unregister();
-	mdp3_iommu_dettach(MDP3_IOMMU_CTX_DMA_0);
+
+	mutex_lock(&mdp3_res->iommu_lock);
+	for (i = 0; i < MDP3_IOMMU_CTX_MAX; i++)
+		mdp3_iommu_dettach(i);
+	mutex_unlock(&mdp3_res->iommu_lock);
+
 	mdp3_iommu_deinit();
 
 	if (!IS_ERR_OR_NULL(mdp3_res->ion_client))
@@ -1627,12 +1628,14 @@ int mdp3_iommu_enable(int client)
 {
 	int rc;
 
+	mutex_lock(&mdp3_res->iommu_lock);
 	if (client == MDP3_CLIENT_DMA_P) {
 		rc = mdp3_iommu_attach(MDP3_IOMMU_CTX_DMA_0);
 	} else {
 		rc = mdp3_iommu_attach(MDP3_IOMMU_CTX_PPP_0);
 		rc |= mdp3_iommu_attach(MDP3_IOMMU_CTX_PPP_1);
 	}
+	mutex_unlock(&mdp3_res->iommu_lock);
 
 	return rc;
 }
@@ -1641,12 +1644,14 @@ int mdp3_iommu_disable(int client)
 {
 	int rc;
 
+	mutex_lock(&mdp3_res->iommu_lock);
 	if (client == MDP3_CLIENT_DMA_P) {
 		rc = mdp3_iommu_dettach(MDP3_IOMMU_CTX_DMA_0);
 	} else {
 		rc = mdp3_iommu_dettach(MDP3_IOMMU_CTX_PPP_0);
 		rc |= mdp3_iommu_dettach(MDP3_IOMMU_CTX_PPP_1);
 	}
+	mutex_unlock(&mdp3_res->iommu_lock);
 
 	return rc;
 }
