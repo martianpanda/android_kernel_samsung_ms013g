dd13f81a8bebdc3959b1a85559cc2e06e4b270dc
Apavayan <info@apavayan.com>
2016-04-10 15:07:24 +0530

    wcnss: Fix compile w/o CONFIG_WCNSS_IRIS_REGISTER_DUMP

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -899,9 +899,7 @@ static void wcnss_log_iris_regs(void)
 		pr_info("[0x%08x] : 0x%08x\n", regs_array[i], reg_val);
 	}
 }
-#endif
 
-#ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 int wcnss_get_mux_control(void)
 {
 	void __iomem *pmu_conf_reg;
@@ -919,7 +917,9 @@ int wcnss_get_mux_control(void)
 	writel_relaxed(reg, pmu_conf_reg);
 	return 1;
 }
+#endif
 
+#ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 void wcnss_log_debug_regs_on_bite(void)
 {
 	struct platform_device *pdev = wcnss_get_platform_device();
@@ -940,8 +940,10 @@ void wcnss_log_debug_regs_on_bite(void)
 
 		if (clk_rate){
 			wcnss_pronto_log_debug_regs();
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 			if (wcnss_get_mux_control())
 				wcnss_log_iris_regs();
+#endif
 		} else {
 			pr_err("clock frequency is zero, cannot access PMU or other registers\n");
 #ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
@@ -957,8 +959,10 @@ void wcnss_reset_intr(void)
 {
 	if (wcnss_hardware_type() == WCNSS_PRONTO_HW) {
 		wcnss_pronto_log_debug_regs();
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 		if (wcnss_get_mux_control())
 			wcnss_log_iris_regs();
+#endif
 		wmb();
 		__raw_writel(1 << 16, penv->fiq_reg);
 	} else {
@@ -976,8 +980,10 @@ void wcnss_reset_fiq(bool clk_chk_en)
 			wcnss_log_debug_regs_on_bite();
 		} else {
 			wcnss_pronto_log_debug_regs();
+#ifdef CONFIG_WCNSS_IRIS_REGISTER_DUMP
 			if (wcnss_get_mux_control())
 				wcnss_log_iris_regs();
+#endif
 		}
 		/* Insert memory barrier before writing fiq register */
 		wmb();
